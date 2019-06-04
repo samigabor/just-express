34e802cc4dfff26e3327f13ddfcb96452cba5ba4
s-gabor <ing.gabor@yahoo.com>
2019-06-04 23:41:05 +0300

    add local strategy

--- expressOAuth/app.js
+++ expressOAuth/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 const passportConfig = require('./config');
 // config.js
@@ -10,6 +11,8 @@ const passportConfig = require('./config');
 //     callbackURL: "http://localhost:3000/auth"
 // }
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use(session({
     secret: 'keyboard cat',
@@ -26,6 +29,12 @@ passport.use(new GitHubStrategy(passportConfig ,
     }
 ));
 
+passport.use(new LocalStrategy((username, password, done) => {
+    console.log('LocalStrategy!!!: ', username, password, done);
+    return done(null, username);
+  }
+));
+
 passport.serializeUser((user, cb) => {
     cb(null, user);
 })
@@ -35,8 +44,8 @@ passport.deserializeUser((user, cb) => {
 })
 
 app.get('/', (req, res) => {
-    // console.log('User data (Please!!!):');
-    // console.log(req.user); // this data gets saved when loging in and stays there during the session(when the server restarts the session is gone)
+    console.log('User data (Please!!!):');
+    console.log(req.body); // this data gets saved when loging in and stays there during the session(when the server restarts the session is gone)
     res.send('works!!');
 })
 
@@ -47,10 +56,16 @@ app.get('/auth', passport.authenticate('github', {
     failureRedirect: '/loginFailed'
 }));
 
-
 app.get('/session', (req, res) => {
     res.send(`My username from the session is: ${req.user.username}`);
 })
 
 
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'})
+);
+
+
 app.listen(3000);
+
